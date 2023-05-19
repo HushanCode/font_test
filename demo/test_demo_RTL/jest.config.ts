c319a4ff27e0ef0b6fe15cccf844e3effa7fcf01
zhuling822 <ling.zhu@js.design>
2023-05-19 15:41:05 +0800

    feat: 初始化RTL

---
+++ demo/test_demo_RTL/jest.config.ts
@@ -0,0 +1,17 @@
+/*
+ * For a detailed explanation regarding each configuration property and type check, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  clearMocks: true,
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest-dom-setup.js"],
+  transform: {
+    "^.+\\.(js|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+    "^.+\\.svg$": "<rootDir>/svg-transform.js",
+  },
+  moduleNameMapper: {
+    "\\.(css|less)$": "identity-obj-proxy",
+  },
+};
